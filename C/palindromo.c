86a0bf1ca68eba9c2610c352b0e33e9e2a526ea5
SamaraneGabriel <samarane.gabriel@gmail.com>
2023-08-22 15:12:36 -0300

    Dependencias

--- C/palindromo.c
+++ C/palindromo.c
@@ -2,12 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+/*@Author: Gabriel Samarane Ribeiro
+ *AEDS II
+ */ 
 
-//funcao que retorna T||F para se a string é um palindromo
+
+
+/*Função para checar se um array de chars representando uma String é palindromo
+ * @Param: Apontador para cadeia de caraters char* str 
+ * @Retorno: Se for palindromo, true, else, false;*/
+
+
 bool isPalindromo(char* str){
 	bool resp = true;
 	for(int i = 0, j = strlen(str)-1; i < strlen(str)/2; i++, j--){
-	        printf("%c,%c",str[i],str[j]);
+		printf("%c, %c",str[i],str[j]);
 		if(str[i] != str[j]){
 			resp = false;
 			i = strlen(str);
@@ -16,13 +26,24 @@ bool isPalindromo(char* str){
 	return resp;
 }
 
+
+
 // metodo principal
+//
 int main(){
 		
 	char* str = (char*) malloc(100*sizeof(char));
+
+	char* ptr = NULL;
+	
 	while(strcmp(str,"FIM") != 0){
-		fgets(str,100,stdin);
-		printf("%s",str);
+		fgets(str,sizeof(str),stdin);
+	
+		ptr = strchr(str,'\n');
+		if(ptr != NULL){
+			*ptr = '\0';
+		}
+
 		if(strcmp(str,"FIM") != 0){
 			if(isPalindromo(str)){
 				printf("SIM\n");
@@ -32,5 +53,6 @@ int main(){
 			}
 		}
 	}
+	free(str);
 	return 0;
 }
